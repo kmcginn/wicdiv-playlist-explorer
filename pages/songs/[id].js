807fd9515438d55e013b1cb5bb65399bd8bbc588
Kevin McGinn <kmcginn14@gmail.com>
2021-04-11 13:42:43 -0700

    implement basic dynamic routes for songs

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,15 +1,29 @@
 import Layout from "../../components/layout"
+import { getPlaylistBasicTrackInfo, getTrackInfo } from "../../lib/spotify-helper"
 
-export async function getStaticProps() {
+export async function getStaticProps({params}) {
+    const trackInfo = await getTrackInfo(params.id);
+
+    // TODO: get audio features
     return {
-        props: {}
+        props: {
+            trackInfo
+        }
     }
 }
 
 export async function getStaticPaths() {
-    const paths = [];
+    const tracks = await getPlaylistBasicTrackInfo();
+
+    const paths = tracks.map((trackInfo) => {
+        return {
+            params: {
+                id: trackInfo.track.id
+            }
+        }
+    });
     return {
-        paths,
+        paths: paths,
         fallback: false
     }
 }
@@ -17,7 +31,7 @@ export async function getStaticPaths() {
 export default function Song(props) {
     return (
         <Layout>
-            
+            <h3>{props.trackInfo.name}</h3>
         </Layout>
     )
 }
