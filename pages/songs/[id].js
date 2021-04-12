a494c3e108d8990fc9a4d3fcaf9de558d18d171d
Kevin McGinn <kmcginn14@gmail.com>
2021-04-11 17:50:15 -0700

    add track info, audio features to song page

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,13 +1,14 @@
 import Layout from "../../components/layout"
-import { getPlaylistBasicTrackInfo, getTrackInfo } from "../../lib/spotify-helper"
+import { getPlaylistBasicTrackInfo, getTrackAudioFeatures, getTrackInfo } from "../../lib/spotify-helper"
 
 export async function getStaticProps({params}) {
     const trackInfo = await getTrackInfo(params.id);
+    const trackAudioFeatures = await getTrackAudioFeatures(params.id);
 
-    // TODO: get audio features
     return {
         props: {
-            trackInfo
+            trackInfo,
+            trackAudioFeatures
         }
     }
 }
@@ -29,9 +30,46 @@ export async function getStaticPaths() {
 }
 
 export default function Song(props) {
+    const albumImgUrl = props.trackInfo.album.images[0]?.url;
+    const albumImgSize = props.trackInfo.album.images[0]?.height;
+
     return (
         <Layout>
-            <h3>{props.trackInfo.name}</h3>
+            <img
+                src={albumImgUrl}
+                alt={"Art for album " + props.trackInfo.album.name}
+                height={albumImgSize}
+                width={albumImgSize}
+                className="object-contain justify-self-center self-center max-h-60"
+            />
+            <h3 className="text-4xl">{props.trackInfo.name}</h3>
+            <h4 className="text-3xl">{props.trackInfo.artists[0].name}</h4>
+            <table className="table-auto text-left">
+                <thead>
+                    <tr>
+                        <th>Feature</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Danceability</td>
+                        <td>{props.trackAudioFeatures.danceability}</td>
+                    </tr>
+                    <tr>
+                        <td>Energy</td>
+                        <td>{props.trackAudioFeatures.energy}</td>
+                    </tr>
+                    <tr>
+                        <td>Valence</td>
+                        <td>{props.trackAudioFeatures.valence}</td>
+                    </tr>
+                    <tr>
+                        <td>Tempo</td>
+                        <td>{props.trackAudioFeatures.tempo}</td>
+                    </tr>
+                </tbody>
+            </table>
         </Layout>
     )
 }
